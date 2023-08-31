fixed the factor 3 scew-up in the phase diagrams'

animate/assym/animate_assym.py
@@ -1,4 +1,4 @@
-from matplotlib import animation
+from matplotlib import animation, cm
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import numpy as np
@@ -12,51 +12,61 @@ plt.rc("font", family="serif", size=16)
 plt.rc("mathtext", fontset="cm")
 plt.rc("lines", lw=2)
 
+rgba_to_hex = lambda rgba : '#'+''.join([f'{int(v*255):02x}' for v in rgba])
+color = rgba_to_hex(cm.viridis(.25))
+
 
 def add_phase(ax, phibar1, phibar2, a):
-    from numpy import pi, sin, cos
-    from matplotlib import cm, ticker, colors as c
+    aa = [0, 0.5, 1, 1.5]
     N = 500
-    kk = 1.4
-    u, v = np.linspace(-kk, kk, N), np.linspace(-kk, kk, N) 
+    L = 1.1
+    u, v = np.linspace(-L, L, N), np.linspace(-L, L, N) 
     u, v = np.meshgrid(u, v)
 
-    rgba_to_hex = lambda rgba : '#'+''.join([f'{int(v*255):02x}' for v in rgba])
-    color = rgba_to_hex(cm.viridis(.25))
-
-    f1 = lambda x, y, a :  ((x**2 - 1) - sqrt(y**4 - a**2 + 0j)).real
-    f2 = lambda x, y, a :  ((x**2 - 1) + sqrt(y**4 - a**2 + 0j)).real
-    x = lambda u, v : sqrt(u**2 + v**2)
-    y = lambda u, v : sqrt(np.abs(u**2 - v**2))
+    f1 = lambda u, v, a :  (- 1 + 3 * (u**2 + v**2) - sqrt((3 * (u**2 - v**2))**2 - a**2 + 0j)).real
+    f2 = lambda u, v, a :  (- 1 + 3 * (u**2 + v**2) + sqrt((3 * (u**2 - v**2))**2 - a**2 + 0j)).real
     f = [f1, f2]
-    g = lambda v, a: sqrt(v**2 + a)
 
-    v0 = np.linspace(0, kk, 500) 
-    u0 = g(v0, a)
-    list1 = [u0, u0, -u0, -u0, v0, v0, -v0, -v0]
-    list2 = [v0, -v0, v0, -v0, u0, -u0, u0, -u0]
-    for l1, l2 in zip(list1, list2):
-        ax.plot(l1, l2, "purple")
+    g = lambda v, a: sqrt(v**2 + a/3)
+    v0 = np.linspace(0, L, 500)
+    sgn1 = [1, 1, -1, -1]
+    sgn2 = [1, -1, 1, -1]
 
-    color = 'black'
-    ls = '--'
-
-    ax.contour(u, v, f1(x(u, v), y(u, v), a), levels=[0], colors=color, linestyles=ls)
-
-    if a>0:
-        lim = max(0,(1 - a)/2)
-        v1 = np.linspace(np.sqrt(lim), np.sqrt(1/2), 100)
-        v2 = np.sqrt(1 - v1**2)
-        sgn1 = [1, 1, -1, -1]
-        sgn2 = [1, -1, 1, -1]
+    for i in range(2):
+        u0 = g(v0, a)
         for s1, s2 in zip(sgn1, sgn2):
-            ax.plot(s1*v1, s2*v2, color="green")
-            ax.plot(s1*v2, s2*v1, color="green")
+            ax.plot(s1*v0, s2*u0, "purple")
+            ax.plot(s1*u0, s2*v0, "purple")
 
-    ax.plot(phibar2, phibar1, 'ro')
+        eig = f[i](u, v, a)
+        if i==0:
+            ax.contour(u, v, eig, levels=[0], colors="black", linestyles="-")
+            ax.contourf(u, v, eig, levels=[np.min(f[i](u, v, a)), 0],  colors=color, alpha=0.3)
+        elif i==1:
+            ax.contour(u, v, eig, levels=[0], colors="blue", linestyles="--")
+            ax.contourf(u, v, eig, levels=[np.min(f[i](u, v, a)), 0],  colors=color, alpha=0.6, hatches=["///"])
 
-    ax.set_xlim(-kk, kk)
-    ax.set_ylim(-kk, kk)
+        if a>0:
+            lim = max(0,(1 - a)/6)
+            v1 = np.linspace(np.sqrt(lim), np.sqrt(1/6), 100)
+            v2 = np.sqrt(1/3 - v1**2)
+
+            for s1, s2 in zip(sgn1, sgn2):
+                ax.plot(s1*v1, s2*v2, color="green")
+                ax.plot(s1*v2, s2*v1, color="green")
+        
+        sq = 1 / sqrt(2)
+        ax.plot([sq, sq, -sq, -sq, sq], [sq, -sq, -sq, sq, sq], 'k--', alpha=.2, lw=3)
+
+        ax.plot(phibar2, phibar1, 'ro')
+
+        ax.set_xlabel("$v_1$")
+        ax.set_ylabel("$v_2$")
+        ax.set_xlim(-L, L)
+        ax.set_ylim(-L, 0.1)
+        ax.set_title("$a = %.1f$"%(a))
+
+
 
 def plot_error(ax, phit, param):
     u, a, b, phibar1, phibar2, N, L, T, dt = param
@@ -72,10 +82,6 @@ def plot_error(ax, phit, param):
     t = np.linspace(0, frames*dt, frames)
     ax2.plot(t, pt[:, 0]-pt[0,0], 'k--', label="$\\varphi_1(t) - \\varphi_1(0)$")
     ax2.plot(t, pt[:, 1]-pt[0,1], 'r--', label="$\\varphi_2(t) - \\varphi_2(0)$")
-    
-    ax.set_ylabel("$\\dot{\\bar\\varphi}$")
-    ax.set_xlabel("$t$")
-    ax2.set_ylabel("$\\Delta\\bar\\varphi$")
 
     ax.legend(loc=3)
     ax2.legend(loc=4)
@@ -84,7 +90,7 @@ def make_anim(folder, filename):
     filename = filename[:-4]
 
     phit, param = load_file(folder, filename)
-    u, a, b, phibar1, phibar2, N, L, T, dt = param
+    u, a, b, phibar1,  phibar2, N, L, T, dt = param
     dx = L / N
     x = np.linspace(0, L, N)
 
@@ -94,13 +100,6 @@ def make_anim(folder, filename):
     ax2 = fig.add_subplot(gs[1:, 1])
     ax3 = fig.add_subplot(gs[1, 0])
     ax4 = fig.add_subplot(gs[2, 0])
-
-    ax1.set_xlabel("$x$")
-    ax1.set_ylabel("$\\varphi$")
-    ax2.set_xlabel("$\\varphi_2$")
-    ax2.set_ylabel("$\\varphi_1$")
-    ax3.set_xlabel("$\\varphi_2$")
-    ax3.set_ylabel("$\\varphi_1$")
 
     ax = [ax1, ax2, ax3]
     fig.suptitle(", ".join(filename_from_param(param).split('_')))
@@ -149,9 +148,9 @@ def make_anim(folder, filename):
             txt = str((m+1)//n3) + "%"
             print(current_process().name, '\t', txt)
 
-    anim = animation.FuncAnimation(fig, animate, cache_frame_data=False,  interval=1, frames=frames//n, repeat=False)
-    # plt.show()
-    anim.save(folder_vid+filename+".mp4", fps=30)
+    anim = animation.FuncAnimation(fig, animate, interval=1, frames=frames//n, repeat=False)
+    plt.show()
+    # anim.save(folder_vid+filename+".mp4", fps=30)
 
 names = [
     "long",
@@ -163,27 +162,23 @@ for name in names:
     folder = "data/assym/" + name + "/"
     folder_vid = "vid/assym/" + name + "/"
 
-
     import os, shutil
     from multiprocessing import Pool, current_process
     if os.path.isdir(folder_vid):
         shutil.rmtree(folder_vid)
-    folders = folder_vid.split("/")
-    for i in range(len(folders)):
-        fol = "/".join(folders[0:i+1]) + "/"
-        if not os.path.isdir(fol):
-            os.mkdir(fol)
-    
+    os.mkdir(folder_vid)
+
     fnames = get_all_filenames_in_folder(folder)
+
 
     import time
     startTime = time.time()
 
-    # [make_anim(folder, fname) for fname in fnames]
+    [make_anim(folder, fname) for fname in fnames[:]]
 
-    folder_fname = [(folder, name) for name in fnames]
-    with Pool(12) as pool:
-        pool.starmap(make_anim, folder_fname)
+    # folder_fname = [(folder, name) for name in fnames]
+    # with Pool(10) as pool:
+    #     pool.starmap(make_anim, folder_fname)
 
     executionTime = (time.time() - startTime)
     print('Execution time in seconds: ' + str(executionTime))

animate/assym/animate_assym_sep.py
@@ -1,4 +1,4 @@
-from matplotlib import animation
+from matplotlib import animation, cm
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import numpy as np
@@ -12,52 +12,59 @@ plt.rc("font", family="serif", size=16)
 plt.rc("mathtext", fontset="cm")
 plt.rc("lines", lw=2)
 
+rgba_to_hex = lambda rgba : '#'+''.join([f'{int(v*255):02x}' for v in rgba])
+color = rgba_to_hex(cm.viridis(.25))
+
 
 def add_phase(ax, phibar1, phibar2, a):
-    from numpy import pi, sin, cos
-    from matplotlib import cm, ticker, colors as c
+    aa = [0, 0.5, 1, 1.5]
     N = 500
-    kk = 1.4
-    u, v = np.linspace(-kk, kk, N), np.linspace(-kk, kk, N) 
+    L = 1.1
+    u, v = np.linspace(-L, L, N), np.linspace(-L, L, N) 
     u, v = np.meshgrid(u, v)
 
-    rgba_to_hex = lambda rgba : '#'+''.join([f'{int(v*255):02x}' for v in rgba])
-    color = rgba_to_hex(cm.viridis(.25))
-
-    f1 = lambda x, y, a :  ((x**2 - 1) - sqrt(y**4 - a**2 + 0j)).real
-    f2 = lambda x, y, a :  ((x**2 - 1) + sqrt(y**4 - a**2 + 0j)).real
-    x = lambda u, v : sqrt(u**2 + v**2)
-    y = lambda u, v : sqrt(np.abs(u**2 - v**2))
+    f1 = lambda u, v, a :  (- 1 + 3 * (u**2 + v**2) - sqrt((3 * (u**2 - v**2))**2 - a**2 + 0j)).real
+    f2 = lambda u, v, a :  (- 1 + 3 * (u**2 + v**2) + sqrt((3 * (u**2 - v**2))**2 - a**2 + 0j)).real
     f = [f1, f2]
-    g = lambda v, a: sqrt(v**2 + a)
 
-    v0 = np.linspace(0, kk, 500) 
-    u0 = g(v0, a)
-    list1 = [u0, u0, -u0, -u0, v0, v0, -v0, -v0]
-    list2 = [v0, -v0, v0, -v0, u0, -u0, u0, -u0]
-    for l1, l2 in zip(list1, list2):
-        ax.plot(l1, l2, "purple")
+    g = lambda v, a: sqrt(v**2 + a/3)
+    v0 = np.linspace(0, L, 500)
+    sgn1 = [1, 1, -1, -1]
+    sgn2 = [1, -1, 1, -1]
 
-    color = 'black'
-    ls = '--'
-
-    ax.contour(u, v, f1(x(u, v), y(u, v), a), levels=[0], colors=color, linestyles=ls)
-
-    if a>0:
-        lim = max(0,(1 - a)/2)
-        v1 = np.linspace(np.sqrt(lim), np.sqrt(1/2), 100)
-        v2 = np.sqrt(1 - v1**2)
-        sgn1 = [1, 1, -1, -1]
-        sgn2 = [1, -1, 1, -1]
+    for i in range(2):
+        u0 = g(v0, a)
         for s1, s2 in zip(sgn1, sgn2):
-            ax.plot(s1*v1, s2*v2, color="green")
-            ax.plot(s1*v2, s2*v1, color="green")
+            ax.plot(s1*v0, s2*u0, "purple")
+            ax.plot(s1*u0, s2*v0, "purple")
 
-    ax.plot(phibar2, phibar1, 'ro')
+        eig = f[i](u, v, a)
+        if i==0:
+            ax.contour(u, v, eig, levels=[0], colors="black", linestyles="-")
+            ax.contourf(u, v, eig, levels=[np.min(f[i](u, v, a)), 0],  colors=color, alpha=0.3)
+        elif i==1:
+            ax.contour(u, v, eig, levels=[0], colors="blue", linestyles="--")
+            ax.contourf(u, v, eig, levels=[np.min(f[i](u, v, a)), 0],  colors=color, alpha=0.6, hatches=["///"])
 
-    ax.set_xlim(-kk, kk)
-    ax.set_ylim(-kk, kk)
+        if a>0:
+            lim = max(0,(1 - a)/6)
+            v1 = np.linspace(np.sqrt(lim), np.sqrt(1/6), 100)
+            v2 = np.sqrt(1/3 - v1**2)
 
+            for s1, s2 in zip(sgn1, sgn2):
+                ax.plot(s1*v1, s2*v2, color="green")
+                ax.plot(s1*v2, s2*v1, color="green")
+        
+        sq = 1 / sqrt(2)
+        ax.plot([sq, sq, -sq, -sq, sq], [sq, -sq, -sq, sq, sq], 'k--', alpha=.2, lw=3)
+
+        ax.plot(phibar2, phibar1, 'ro')
+
+        ax.set_xlabel("$v_1$")
+        ax.set_ylabel("$v_2$")
+        ax.set_xlim(-L, L)
+        ax.set_ylim(-L, 0.1)
+        ax.set_title("$a = %.1f$"%(a))
 
 
 def plot_error(ax, phit, param):